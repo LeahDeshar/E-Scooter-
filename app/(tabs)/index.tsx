9eca0b5f2efabe3dd7711b87cc1fd861a14b8fde
Leah Deshar <leah.desar@gmail.com>
2024-12-27 09:31:28 +0545

    chore: Add scooter markers to home screen map

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,11 @@ import { Image, StyleSheet, Platform, View, Animated } from "react-native";
 import React, { useRef, useEffect, useState } from "react";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from "expo-location";
-
+import scooters from "../../data/scooters.json";icon: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain', // Scales the image properly
+  },
 export default function HomeScreen() {
   const mapRef = useRef(null);
   const [location, setLocation] = useState(null);
@@ -120,6 +124,22 @@ export default function HomeScreen() {
             </View>
           </Marker>
         )}
+
+        {scooters.map((scooter) => (
+          <Marker
+            key={scooter.id}
+            coordinate={{
+              latitude: scooter.latitude,
+              longitude: scooter.longitude,
+            }}
+            anchor={{ x: 0.5, y: 1 }} // Equivalent to iconAnchor: 'bottom'
+          >
+            <Image
+              source={require("./assets/pin.png")} // Replace with your pin icon
+              style={styles.icon}
+            />
+          </Marker>
+        ))}
       </MapView>
     </View>
   );
@@ -132,7 +152,11 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
-
+icon: {
+    width: 25,
+    height: 25,
+    resizeMode: 'contain', // Scales the image properly
+  },
   puckContainer: {
     alignItems: "center",
     justifyContent: "center",
